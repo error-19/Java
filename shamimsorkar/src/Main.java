714083440e677f2fdf96ce818afabf7d60140546
unknown <mdshamimsorkar19@gmail.com>
2023-10-29 10:47:46 +0600

    First commit Message

---
+++ shamimsorkar/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Define two numbers to be added
+        double number1 = 5.5;
+        double number2 = 3.2;
+
+        // Perform the addition
+        double sum = number1 + number2;
+
+        // Print the result
+        System.out.println( + number1 +  + number2 + " is: " + sum);
+    }
+}
