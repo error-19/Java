62aa0548f444f2ea64a7a5cc5b9e68a42d80af7f
USER <mdshamimsorkar19@gmail.com>
2023-11-16 23:25:34 +0600

    assaingment-1

--- LoginApplication.java
+++ LoginApplication.java
@@ -1,12 +1,22 @@
 package com.hrm2live;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 public class LoginApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\USER\\Documents\\Drivers\\geckodriver.exe");
         WebDriver driver=new FirefoxDriver();
-
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Thread.sleep(500);
     }
 }
